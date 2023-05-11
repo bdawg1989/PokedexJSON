ccecf583c6510826f9e18d20890b54405b7db1c9
Gengar <80122551+bdawg1989@users.noreply.github.com>
2023-05-11 16:51:32 -0500

    Update extractor.py
    Added Learned Moves to the extraction process.
    Added Progress bar inside console when running script.

--- extractor.py
+++ extractor.py
@@ -1,6 +1,80 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+
+####################
+##  Sanitize Name ##
+####################
+def sanitize_name(name):
+    name = name.replace(" ", "-").replace(".", "").replace(":", "").replace("♀", "-f").replace("♂", "-m")
+    name = re.sub(r"[^a-zA-Z0-9\-]", "", name)  # Remove any remaining special characters
+    name = name.replace("é", "e")  # Replace "é" with "e"
+    return name.lower()
+
+
+####################
+##   GET MOVES    ##
+####################
+def get_moves(soup):
+    moves_table = soup.find("table", class_="data-table")
+    move_rows = moves_table.find_all("tr")[1:]
+
+    moves = []
+    for row in move_rows:
+        cells = row.find_all("td")
+        level = int(cells[0].text)
+        move = cells[1].find("a", class_="ent-name").text
+        move_type = cells[2].find("a", class_="type-icon").text
+        power = cells[4].text
+        accuracy = cells[5].text
+
+        moves.append({
+            "level": level,
+            "move": move,
+            "type": move_type,
+            "power": int(power) if power.isdigit() else None,
+            "accuracy": int(accuracy) if accuracy.isdigit() else None
+        })
+
+    return moves
+    
+####################
+##  GET DETAILS   ##
+####################      
+def get_pokemon_details(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    table = soup.find("table", class_="vitals-table")
+    
+    if not table:
+        print(f"Error: Table not found for URL: {url}")
+        return None
+
+    rows = table.find_all("tr")
+    local_no_row = table.find("th", string="Local №")
+    if local_no_row:
+        local_no_cell = local_no_row.find_next_sibling("td")
+        local_nos = re.findall(r'\d+ .+?\)', local_no_cell.text)  # Find all sets of parentheses
+        local_no = '\n'.join(local_nos)  # Join them with newline character
+    species = rows[2].find("td").text
+    height = rows[3].find("td").text
+    weight = rows[4].find("td").text
+    abilities = [a.text for a in rows[5].find("td").find_all("a")]
+    local_no = rows[6].find("td").text.strip()
+    moves = get_moves(soup)
+
+    details = {
+        "species": species,
+        "height": height,
+        "weight": weight,
+        "abilities": abilities,
+        "local_no": local_no,
+        "moves": moves  # Add the moves key
+    }
+
+    return details
 
 url = "https://pokemondb.net/pokedex/all"
 response = requests.get(url)
@@ -41,5 +115,23 @@ for row in rows[1:]:
 
     pokemon_list.append(pokemon)
 
+total_pokemon = len(pokemon_list)
+completed_pokemon = 0
+
 with open("pokemon_data.json", "w") as outfile:
-    json.dump(pokemon_list, outfile, indent=4)
+    for i, pokemon in enumerate(pokemon_list):
+        name = pokemon["name"]
+        sanitized_name = sanitize_name(name)
+        pokedex_url = f"https://pokemondb.net/pokedex/{sanitized_name}"
+        details = get_pokemon_details(pokedex_url)
+
+        if details:
+            pokemon.update(details)
+            completed_pokemon += 1
+            percentage = (completed_pokemon / total_pokemon) * 100
+            print(f"Progress: {completed_pokemon}/{total_pokemon} ({percentage:.2f}%) - {name}")
+
+        json.dump(pokemon, outfile, indent=4)
+
+    print("Data extraction completed.")
+
