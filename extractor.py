a6745f6ff70ee33db2654bb57f4669dd86514d93
Gengar <80122551+bdawg1989@users.noreply.github.com>
2023-05-11 16:01:54 -0500

    Update extractor.py
    Updated to get more info.

--- extractor.py
+++ extractor.py
@@ -2,19 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-URL = "https://pokemondb.net/pokedex/all"
-page = requests.get(URL)
+url = "https://pokemondb.net/pokedex/all"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(page.content, "html.parser")
+pokemon_list = []
 
-tbody = soup.find("tbody")
-trs = tbody.find_all("tr")
-pokedex = {}
-for tr in trs:
-    tds = tr.find_all("td")
-    pid = tds[0].text
-    if pid not in pokedex:
-        pokedex[pid] = {"name": tds[1].find("a").text, "photo":"https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + pid + ".png", "desc":"https://www.pokemon.com/us/pokedex/" + pid, "types": list(filter(None, tds[2].text.split(" ")))}
+table = soup.find("table", {"id": "pokedex"})
+rows = table.find_all("tr")
 
-with open("newPokedex.json", "w") as f:
-    json.dump(pokedex, f, indent=4)
+for row in rows[1:]:
+    cells = row.find_all("td")
+    image_url = cells[0].find("img")["src"]
+    dex_number = int(cells[0].find("span", class_="infocard-cell-data").text)
+    name = cells[1].find("a", class_="ent-name").text
+    types = [typ.text for typ in cells[2].find_all("a")]
+    total = int(cells[3].text)
+    hp = int(cells[4].text)
+    attack = int(cells[5].text)
+    defense = int(cells[6].text)
+    sp_atk = int(cells[7].text)
+    sp_def = int(cells[8].text)
+    speed = int(cells[9].text)
+
+    pokemon = {
+        "image": image_url,
+        "dex_number": dex_number,
+        "name": name,
+        "type": types,
+        "total": total,
+        "hp": hp,
+        "attack": attack,
+        "defense": defense,
+        "sp_atk": sp_atk,
+        "sp_def": sp_def,
+        "speed": speed,
+    }
+
+    pokemon_list.append(pokemon)
+
+with open("pokemon_data.json", "w") as outfile:
+    json.dump(pokemon_list, outfile, indent=4)
