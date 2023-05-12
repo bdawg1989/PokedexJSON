826cceb96850e2f0f02d8802920f93e43eb37d8e
Gengar <80122551+bdawg1989@users.noreply.github.com>
2023-05-12 10:51:53 -0500

    Update extractor.py
    Added all moves to the json data.  Level Moves, Egg Moves, and TM Moves. 
    Skipped different forms when generating json data. 
    Fixed json data not being compiled correctly.

--- extractor.py
+++ extractor.py
@@ -8,41 +8,67 @@ import re
 ####################
 def sanitize_name(name):
     name = name.replace(" ", "-").replace(".", "").replace(":", "").replace("♀", "-f").replace("♂", "-m")
-    name = re.sub(r"[^a-zA-Z0-9\-]", "", name)  # Remove any remaining special characters
-    name = name.replace("é", "e")  # Replace "é" with "e"
+    name = name.replace("é", "e")
+    name = re.sub(r"[^a-zA-Z0-9\-]", "", name) 
     return name.lower()
+
+
+
+####################
+## GET MOVES FROM TABLE ##
+####################
+def get_moves_from_table(moves_table, level_key=None):
+    move_rows = moves_table.find_all("tr")[1:]
+
+    moves = []
+    for row in move_rows:
+        cells = row.find_all("td")
+        level = int(cells[level_key].text) if level_key is not None else None
+        move_link = cells[1].find("a", class_="ent-name")
+        if not move_link:
+            continue
+        move = move_link.text
+        move_type = cells[2].find("a", class_="type-icon").text
+        power = cells[4].text
+        accuracy = cells[5].text
+
+        move_data = {
+            "level": level,
+            "move": move,
+            "type": move_type,
+            "power": int(power) if power.isdigit() else None,
+            "accuracy": int(accuracy) if accuracy.isdigit() else None
+        }
+        moves.append(move_data)
+
+    return moves
 
 
 ####################
 ##   GET MOVES    ##
 ####################
 def get_moves(soup):
-    moves_table = soup.find("table", class_="data-table")
-    move_rows = moves_table.find_all("tr")[1:]
+    level_up_table = soup.find_all("table", class_="data-table")[0]
+    tm_table = soup.find_all("table", class_="data-table")[1]
+    egg_moves_table = soup.find_all("table", class_="data-table")[2]
 
-    moves = []
-    for row in move_rows:
-        cells = row.find_all("td")
-        level = int(cells[0].text)
-        move = cells[1].find("a", class_="ent-name").text
-        move_type = cells[2].find("a", class_="type-icon").text
-        power = cells[4].text
-        accuracy = cells[5].text
+    level_up_moves = get_moves_from_table(level_up_table, level_key=0)
+    tm_moves = get_moves_from_table(tm_table)
+    egg_moves = get_moves_from_table(egg_moves_table)
 
-        moves.append({
-            "level": level,
-            "move": move,
-            "type": move_type,
-            "power": int(power) if power.isdigit() else None,
-            "accuracy": int(accuracy) if accuracy.isdigit() else None
-        })
+    return level_up_moves + tm_moves + egg_moves
 
-    return moves
+
+
+
     
 ####################
 ##  GET DETAILS   ##
 ####################      
 def get_pokemon_details(url):
+    if '-' in url.split('/')[-1]:
+        return None
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -56,8 +82,8 @@ def get_pokemon_details(url):
     local_no_row = table.find("th", string="Local №")
     if local_no_row:
         local_no_cell = local_no_row.find_next_sibling("td")
-        local_nos = re.findall(r'\d+ .+?\)', local_no_cell.text)  # Find all sets of parentheses
-        local_no = '\n'.join(local_nos)  # Join them with newline character
+        local_nos = re.findall(r'\d+ .+?\)', local_no_cell.text)  
+        local_no = '\n'.join(local_nos) 
     species = rows[2].find("td").text
     height = rows[3].find("td").text
     weight = rows[4].find("td").text
@@ -71,7 +97,7 @@ def get_pokemon_details(url):
         "weight": weight,
         "abilities": abilities,
         "local_no": local_no,
-        "moves": moves  # Add the moves key
+        "moves": moves  
     }
 
     return details
@@ -90,6 +116,9 @@ for row in rows[1:]:
     image_url = cells[0].find("img")["src"]
     dex_number = int(cells[0].find("span", class_="infocard-cell-data").text)
     name = cells[1].find("a", class_="ent-name").text
+    form_name_element = cells[1].find("small", class_="text-muted")
+    if form_name_element:
+        name += f" ({form_name_element.text})"
     types = [typ.text for typ in cells[2].find_all("a")]
     total = int(cells[3].text)
     hp = int(cells[4].text)
@@ -115,8 +144,12 @@ for row in rows[1:]:
 
     pokemon_list.append(pokemon)
 
+
 total_pokemon = len(pokemon_list)
 completed_pokemon = 0
+
+# Create an empty list to store completed Pokémon dictionaries.
+completed_pokemon_list = []
 
 with open("pokemon_data.json", "w") as outfile:
     for i, pokemon in enumerate(pokemon_list):
@@ -127,11 +160,14 @@ with open("pokemon_data.json", "w") as outfile:
 
         if details:
             pokemon.update(details)
+    
+            completed_pokemon_list.append(pokemon)
             completed_pokemon += 1
             percentage = (completed_pokemon / total_pokemon) * 100
             print(f"Progress: {completed_pokemon}/{total_pokemon} ({percentage:.2f}%) - {name}")
 
-        json.dump(pokemon, outfile, indent=4)
+    # Dump the completed_pokemon_list into the JSON file.
+    json.dump(completed_pokemon_list, outfile, indent=4)
 
     print("Data extraction completed.")
 
